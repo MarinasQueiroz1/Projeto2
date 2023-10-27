749f5dab9002b7e3a069c0031da7d101f85a8196
Leonardo Moreira <pirulitodepalmito09@gmail.com>
2023-10-27 15:19:16 -0300

    Esteira

---
+++ Documents/NetBeansProjects/projete1/src/main/java/ifce/projete1/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifce.projete1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Principal {
+    
+    public static void main(String[] args){
+        // DEFINIR E INICIALIZAR AS VARIAVEIS ]
+        Esteira rl = new Esteira();
+        el.ligar();
+        el.desligar();
+        
+        String s=JOptionPane.showInputDialog("entre com um valor");
+        
+        System.out.println(Integer.valueOf(s)+"valor");
+        
+    }
+    
+}
